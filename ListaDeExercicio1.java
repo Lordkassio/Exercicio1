9301b30425af5ccaf656478e790230a346ce6c2f
Cássio Nunes <cassionunes.4951@aluno.una.br>
2022-10-04 18:46:51 -0300

    First commit

---
+++ ListaDeExercicio1.java
@@ -0,0 +1,10 @@
+package ListaDeExerciciosPraPraticar;
+
+public class ListaDeExercicio1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Olá mundo");
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Exercício1 - Faça um Programa que mostre a mensagem "Alo mundo" na tela.
